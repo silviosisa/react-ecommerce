caeaab7c4187040cb328a26182fad10af2adc9e5
Silvio Sisa <silviosisa@gmail.com>
2021-01-30 21:23:49 -0300

    email and password auth firebase

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './styles.scss';
 import Logo from './../../assets/logo.png';
 import { Link } from "react-router-dom";
+import { auth } from './../../firebase/utils';
 
-const Header = () => {
+const Header = props => {
+    const { currentUser } = props;
     return (
         <header className="header">
             <div className="wrap">
@@ -13,15 +15,32 @@ const Header = () => {
                    </Link>
                 </div>
                 <div className="callToActions">
-                    <ul>
-                        <li>
-                            <Link to="/registration">Register</Link>
-                        </li>
-                    </ul>
+                    {currentUser && (
+                        <ul>
+                            <li>
+                                <span onClick={() => auth.signOut()}>Logout</span>
+                            </li>
+                        </ul>
+                    )}
+                    {!currentUser && (
+                        <ul>
+                            <li>
+                                <Link to="/registration">Register</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                        </ul>
+                    )}
                 </div>
             </div>
         </header>
     );
 };
+
+Header.defaultProps = {
+   currentUser: null
+}
+
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsTTJeWakri57tJTBcTkulTN-E-aHwfeE",
+    authDomain: "ecommerce-website-7a92c.firebaseapp.com",
+    projectId: "ecommerce-website-7a92c",
+    storageBucket: "ecommerce-website-7a92c.appspot.com",
+    messagingSenderId: "143268672872",
+    appId: "1:143268672872:web:f7778c58c09d3d3b8492dd"
+}
